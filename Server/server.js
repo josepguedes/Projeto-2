e958579e9368487e2ab29961b955142e56bd700d
Edi <eduardo.mb.sousa25@gmail.com>
2025-05-21 11:55:12 +0100

    Alterar nome

--- Server/server.js
+++ Server/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 // middleware for CORS (Cross-Origin Resource Sharing)
 app.use('/anuncios', require('./routes/anuncios.routes.js'));
 app.use('/denuncias', require('./routes/denuncias.routes.js'));
-app.use('/avaliacoes', require('./routes/avaliaçoes.routes.js'));
+app.use('/avaliacoes', require('./routes/avaliacoes.routes.js'));
 app.use('/utilizadores', require('./routes/utilizadores.routes.js'));
 app.use('/notificacoes', require('./routes/notificacoes.routes.js'));
 
